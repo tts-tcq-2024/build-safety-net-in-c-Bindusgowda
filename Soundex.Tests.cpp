73f5128ee31e7ae073e113088728ff6f4bcd1989
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-09-02 18:27:45 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,31 @@
-#include <gtest/gtest.h>
-#include "Soundex.h"
+#include <assert.h>
+#include <stdio.h>
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+void testGenerateSoundex(const char *name, const char *expectedSoundex) {
+    char soundex[5];
+    generateSoundex(name, soundex);
+    assert(strcmp(soundex, expectedSoundex) == 0);
 }
+
+int main() {
+    
+    testGenerateSoundex("Robert", "R163");
+    testGenerateSoundex("Rupert", "R163");
+    testGenerateSoundex("Rubin", "R150");
+    testGenerateSoundex("Ashcraft", "A261");
+    testGenerateSoundex("Tymczak", "T252");
+
+    // Edge Cases
+    testGenerateSoundex("", "0000");
+    testGenerateSoundex("Xyz", "X020");
+    testGenerateSoundex("123", "0000");  
+
+    testGenerateSoundex("robert", "R163");
+    testGenerateSoundex("RuBeRt", "R163");
+    testGenerateSoundex("ASHCRAFT", "A261");
+
+    testGenerateSoundex("Alexander", "A425");
+
+    return 0;
+}
+
