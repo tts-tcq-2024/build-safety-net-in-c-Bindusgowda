3de58742e69b315a2d3a8dea55dd020b1a324777
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-09-02 18:45:39 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,11 +1,11 @@
 #include <ctype.h>
 #include<string.h>
-
+ 
 typedef struct {
     char sound;
     char value;
 } SoundexMapping;
-
+ 
 static const SoundexMapping soundexMappings[] = {
     {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
     {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'},
@@ -15,44 +15,42 @@ static const SoundexMapping soundexMappings[] = {
     {'M', '5'}, {'N', '5'},
     {'R', '6'}
 };
-
+ 
 #define MAPPING_COUNT (sizeof(soundexMappings) / sizeof(SoundexMapping))
-
+ 
 char getSoundexCode(char c) {
     c = toupper(c);
-
+ 
     for (int i = 0; i < MAPPING_COUNT; i++) {
         if (soundexMappings[i].sound == c) {
             return soundexMappings[i].value;
         }
     }
-
+ 
         return '0'; 
 }
-
+ 
 char getValidCode(char currentCode, char lastCode) {
     if (currentCode != '0' && currentCode != lastCode) {
         return currentCode;
     }
     return '0';
 }
-
+ 
 void generateSoundex(const char *name, char *soundex) {
-   
     soundex[0] = toupper(name[0]);
     soundex[1] = soundex[2] = soundex[3] = '0'; 
     soundex[4] = '\0'; 
     char lastCode = '0';
     int soundexIndex = 1;
-
+ 
     for (int i = 1; name[i] != '\0' && soundexIndex < 4; ++i) {
         char currentCode = getSoundexCode(name[i]);
         char validCode = getValidCode(currentCode, lastCode);
-
+ 
         if (validCode != '0') {
             soundex[soundexIndex++] = validCode;
             lastCode = validCode;
         }
     }
 }
-
