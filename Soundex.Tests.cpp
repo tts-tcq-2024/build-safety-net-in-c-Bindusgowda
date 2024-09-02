f2f1631b19461faf399c22f838fcb34b40b61971
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-09-02 18:57:52 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include "soundex.h"
+#include "Soundex.h"
 
 void testSoundex(const char *input, const char *expected) {
     char soundex[5];
