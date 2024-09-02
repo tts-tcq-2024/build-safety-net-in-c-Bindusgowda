f9be9333f126197298bfcaea875addc2d8dfd9e1
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-09-02 17:27:11 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,105 +1,58 @@
-#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
+#include<string.h>
 
-#define SOUNDDEX_LENGTH 4
+typedef struct {
+    char sound;
+    char value;
+} SoundexMapping;
 
-//array mapping
-static const char soundexMapping[26] = {
-    '0', // A
-    '1', // B
-    '0', // C
-    '0', // D
-    '0', // E
-    '1', // F
-    '2', // G
-    '0', // H
-    '0', // I
-    '1', // J
-    '2', // K
-    '4', // L
-    '5', // M
-    '5', // N
-    '0', // O
-    '1', // P
-    '2', // Q
-    '6', // R
-    '2', // S
-    '3', // T
-    '0', // U
-    '1', // V
-    '0', // W
-    '2', // X
-    '0', // Y
-    '2'  // Z
+static const SoundexMapping soundexMappings[] = {
+    {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
+    {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'},
+    {'S', '2'}, {'X', '2'}, {'Z', '2'},
+    {'D', '3'}, {'T', '3'},
+    {'L', '4'},
+    {'M', '5'}, {'N', '5'},
+    {'R', '6'}
 };
 
+#define MAPPING_COUNT (sizeof(soundexMappings) / sizeof(SoundexMapping))
 
 char getSoundexCode(char c) {
-    if (isalpha(c)) {
-        c = toupper(c);
-        return soundexMapping[c - 'A'];
-    }
-    return '0'; // For non-alphabetic characters
-}
+    c = toupper(c);
 
-void initializeSoundex(const char* name, char* soundex) {
-    if (name[0] != '\0') {
-        soundex[0] = toupper(name[0]);
-        char secondex = getSoundexCode(name[1]);
-        soundex[1] = (secondex != '0') ? secondex : '\0';
-        soundex[2] = '\0'; // Null-terminate the string
-    }
-}
-
-
-char getFirstCharCode(const char* name) {
-    if (name[0] != '\0') {
-        return toupper(name[0]);
-    }
-    return '\0';
-}
-
-
-void addSoundexCode(char* soundex, char code, char* prevCode) {
-    if (code != '0' && code != *prevCode) {
-        size_t len = strlen(soundex);
-        if (len < SOUNDDEX_LENGTH - 1) {
-            soundex[len] = code;
-            soundex[len + 1] = '\0';
+    for (int i = 0; i < MAPPING_COUNT; i++) {
+        if (soundexMappings[i].sound == c) {
+            return soundexMappings[i].value;
         }
-        *prevCode = code;
+    }
+
+        return '0'; 
+}
+
+char getValidCode(char currentCode, char lastCode) {
+    if (currentCode != '0' && currentCode != lastCode) {
+        return currentCode;
+    }
+    return '0';
+}
+
+void generateSoundex(const char *name, char *soundex) {
+   
+    soundex[0] = toupper(name[0]);
+    soundex[1] = soundex[2] = soundex[3] = '0'; 
+    soundex[4] = '\0'; 
+    char lastCode = '0';
+    int soundexIndex = 1;
+
+    for (int i = 1; name[i] != '\0' && soundexIndex < 4; ++i) {
+        char currentCode = getSoundexCode(name[i]);
+        char validCode = getValidCode(currentCode, lastCode);
+
+        if (validCode != '0') {
+            soundex[soundexIndex++] = validCode;
+            lastCode = validCode;
+        }
     }
 }
 
-
-void processName(const char* name, char* soundex) {
-    initializeSoundex(name, soundex);
-    char prevCode = soundex[1];
-    
-    for (size_t i = 2; i < strlen(name) && strlen(soundex) < SOUNDDEX_LENGTH; ++i) {
-        char code = getSoundexCode(name[i]);
-        addSoundexCode(soundex, code, &prevCode);
-    }
-}
-
-
-void padSoundex(char* soundex) {
-    size_t len = strlen(soundex);
-    while (len < SOUNDDEX_LENGTH - 1) {
-        soundex[len++] = '0';
-    }
-    soundex[SOUNDDEX_LENGTH - 1] = '\0'; // Null-terminate
-}
-
-void generateSoundex(const char* name, char* result) {
-    if (name[0] != '\0') {
-        char processedName[SOUNDDEX_LENGTH] = {0};
-        char firstChar = getFirstCharCode(name);
-        processName(name, processedName);
-        padSoundex(processedName);
-        strcpy(result, processedName);
-    } else {
-        result[0] = '\0'; // Empty result for empty name
-    }
-}
