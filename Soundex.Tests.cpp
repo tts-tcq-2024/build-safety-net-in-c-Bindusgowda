cc1c15998d1ae8c135951217c5c91227faecaa34
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-09-03 12:44:46 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,74 +1,124 @@
-#include <CUnit/CUnit.h>
-#include <CUnit/Basic.h>
-#include <ctype.h>
-#include <string.h>
+#include <gtest/gtest.h>
+#include <cstring>
 #include "Soundex.h"
 
-// Test cases
-void test_getSoundexCode(void) {
-    CU_ASSERT_EQUAL(getSoundexCode('B'), '1');
-    CU_ASSERT_EQUAL(getSoundexCode('F'), '1');
-    CU_ASSERT_EQUAL(getSoundexCode('P'), '1');
-    CU_ASSERT_EQUAL(getSoundexCode('V'), '1');
-    CU_ASSERT_EQUAL(getSoundexCode('C'), '2');
-    CU_ASSERT_EQUAL(getSoundexCode('G'), '2');
-    CU_ASSERT_EQUAL(getSoundexCode('J'), '2');
-    CU_ASSERT_EQUAL(getSoundexCode('K'), '2');
-    CU_ASSERT_EQUAL(getSoundexCode('Q'), '2');
-    CU_ASSERT_EQUAL(getSoundexCode('S'), '2');
-    CU_ASSERT_EQUAL(getSoundexCode('X'), '2');
-    CU_ASSERT_EQUAL(getSoundexCode('Z'), '2');
-    CU_ASSERT_EQUAL(getSoundexCode('D'), '3');
-    CU_ASSERT_EQUAL(getSoundexCode('T'), '3');
-    CU_ASSERT_EQUAL(getSoundexCode('L'), '4');
-    CU_ASSERT_EQUAL(getSoundexCode('M'), '5');
-    CU_ASSERT_EQUAL(getSoundexCode('N'), '5');
-    CU_ASSERT_EQUAL(getSoundexCode('R'), '6');
-    CU_ASSERT_EQUAL(getSoundexCode('A'), '0'); // testing a non-mapped character
+// Test get_Soundex_Code function
+TEST(SoundexTest, get_Soundex_Code) {
+    EXPECT_EQ('1', get_Soundex_Code('B'));
+    EXPECT_EQ('1', get_Soundex_Code('F'));
+    EXPECT_EQ('1', get_Soundex_Code('P'));
+    EXPECT_EQ('1', get_Soundex_Code('V'));
+
+    EXPECT_EQ('2', get_Soundex_Code('C'));
+    EXPECT_EQ('2', get_Soundex_Code('G'));
+    EXPECT_EQ('2', get_Soundex_Code('J'));
+    EXPECT_EQ('2', get_Soundex_Code('K'));
+    EXPECT_EQ('2', get_Soundex_Code('Q'));
+    EXPECT_EQ('2', get_Soundex_Code('S'));
+    EXPECT_EQ('2', get_Soundex_Code('X'));
+    EXPECT_EQ('2', get_Soundex_Code('Z'));
+
+    EXPECT_EQ('3', get_Soundex_Code('D'));
+    EXPECT_EQ('3', get_Soundex_Code('T'));
+
+    EXPECT_EQ('4', get_Soundex_Code('L'));
+
+    EXPECT_EQ('5', get_Soundex_Code('M'));
+    EXPECT_EQ('5', get_Soundex_Code('N'));
+
+    EXPECT_EQ('6', get_Soundex_Code('R'));
+
+    EXPECT_EQ('0', get_Soundex_Code('A'));
+    EXPECT_EQ('0', get_Soundex_Code('E'));
+    EXPECT_EQ('0', get_Soundex_Code('I'));
+    EXPECT_EQ('0', get_Soundex_Code('O'));
+    EXPECT_EQ('0', get_Soundex_Code('U'));
+
+    EXPECT_EQ('0', get_Soundex_Code('H'));
+    EXPECT_EQ('0', get_Soundex_Code('W'));
+    EXPECT_EQ('0', get_Soundex_Code('Y'));
+
+    EXPECT_EQ(get_Soundex_Code('b'), get_Soundex_Code('B'));
+    EXPECT_EQ(get_Soundex_Code('c'), get_Soundex_Code('C'));
+    EXPECT_EQ(get_Soundex_Code('d'), get_Soundex_Code('D'));
+    EXPECT_EQ(get_Soundex_Code('r'), get_Soundex_Code('R'));
 }
 
-void test_getValidCode(void) {
-    CU_ASSERT_EQUAL(getValidCode('1', '0'), '1');
-    CU_ASSERT_EQUAL(getValidCode('1', '1'), '0');
-    CU_ASSERT_EQUAL(getValidCode('2', '1'), '2');
-    CU_ASSERT_EQUAL(getValidCode('2', '2'), '0');
-    CU_ASSERT_EQUAL(getValidCode('3', '2'), '3');
-    CU_ASSERT_EQUAL(getValidCode('0', '3'), '0');
+// Test fetch_first_char function
+TEST(SoundexTest, FetchFirstChar) {
+    EXPECT_EQ(fetch_first_char("Example"), 'E');
+    EXPECT_EQ(fetch_first_char("soundex"), 'S');
+    EXPECT_EQ(fetch_first_char(""), '\0');
 }
 
-void test_generateSoundex(void) {
-    char result[5];
+// Test append_Soundex function
+TEST(SoundexTest, AppendSoundex) {
+    char soundex[5] = "A";
+    char prevCode = '1';
+    int length = 1;
 
-    generateSoundex("Robert", result);
-    CU_ASSERT_STRING_EQUAL(result, "R163");
+    append_Soundex(soundex, '2', &prevCode, &length);
+    EXPECT_STREQ(soundex, "A2");
+    EXPECT_EQ(length, 2);
 
-    generateSoundex("Jackson", result);
-    CU_ASSERT_STRING_EQUAL(result, "J250");
+    append_Soundex(soundex, '3', &prevCode, &length);
+    EXPECT_STREQ(soundex, "A23");
+    EXPECT_EQ(length, 3);
 
-    generateSoundex("Smith", result);
-    CU_ASSERT_STRING_EQUAL(result, "S530");
-
-    generateSoundex("Fritz", result);
-    CU_ASSERT_STRING_EQUAL(result, "F320");
-
-    generateSoundex("MacDonald", result);
-    CU_ASSERT_STRING_EQUAL(result, "M325");
-
-    generateSoundex("", result); // Edge case: empty input
-    CU_ASSERT_STRING_EQUAL(result, "0000");
+    append_Soundex(soundex, '3', &prevCode, &length);  // Should not append same code
+    EXPECT_STREQ(soundex, "A23");
+    EXPECT_EQ(length, 3);
 }
 
-int main() {
-    CU_initialize_registry();
-    CU_pSuite pSuite = CU_add_suite("SoundexTestSuite", 0, 0);
+// Test initialize_Soundex function
+TEST(SoundexTest, InitializeSoundex) {
+    char soundex[5] = "";
+    int length = 0;
 
-    CU_add_test(pSuite, "test_getSoundexCode", test_getSoundexCode);
-    CU_add_test(pSuite, "test_getValidCode", test_getValidCode);
-    CU_add_test(pSuite, "test_generateSoundex", test_generateSoundex);
+    initialize_Soundex("Smith", 'S', soundex, &length);
+    EXPECT_STREQ(soundex, "S5");
+    EXPECT_EQ(length, 2);
+}
 
-    CU_basic_set_mode(CU_BRM_VERBOSE);
-    CU_basic_run_tests();
-    CU_cleanup_registry();
-    
-    return 0;
+// Test process_Soundex function
+TEST(SoundexTest, ProcessSoundex) {
+    char soundex[5] = "";
+    int length = 0;
+
+    process_Soundex("Smith", 'S', soundex, &length);
+    EXPECT_STREQ(soundex, "S53");
+    EXPECT_EQ(length, 3);
+}
+
+// Test padding_Soundex function
+TEST(SoundexTest, PaddingSoundex) {
+    char soundex[5] = "S5";
+
+    padding_Soundex(soundex);
+    EXPECT_STREQ(soundex, "S500");
+
+    strcpy(soundex, "S53");
+    padding_Soundex(soundex);
+    EXPECT_STREQ(soundex, "S530");
+
+ }
+
+// Test generate_Soundex function
+TEST(SoundexTest, GenerateSoundex) {
+    char soundex[5] = "";
+
+    generate_Soundex("Smith", soundex);
+    EXPECT_STREQ(soundex, "S530");
+
+    generate_Soundex("Smythe", soundex);
+    EXPECT_STREQ(soundex, "S530");
+
+    generate_Soundex("Ashcraft", soundex);
+    EXPECT_STREQ(soundex, "A261");
+
+    generate_Soundex("Tymczak", soundex);
+    EXPECT_STREQ(soundex, "T520");
+
+    generate_Soundex("", soundex);
+    EXPECT_STREQ(soundex, "");
 }
