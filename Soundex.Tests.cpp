575be1cd28c29521ede048d8b0e8458356852ff0
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-09-03 14:43:32 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,50 +2,23 @@
 #include <string.h>
 #include <ctype.h>
 #include "Soundex.h"
-
-void test_getSoundexCode();
-void test_getValidCode();
-void test_generateSoundex();
-
-int main() {
-    test_getSoundexCode();
-    test_getValidCode();
-    test_generateSoundex();
-    return 0;
-}
-
-void test_getSoundexCode() {
-
-    getSoundexCode('B'); 
-    getSoundexCode('J'); 
-    getSoundexCode('L'); 
-    getSoundexCode('M'); 
-
-    getSoundexCode('A'); 
-    getSoundexCode(' '); 
-    
-    getSoundexCode('b'); 
-    getSoundexCode('j'); 
-}
-
-void test_getValidCode() {
-
-    getValidCode('1', '2');
-
-    getValidCode('2', '2'); 
-
-    getValidCode('0', '1'); 
-
-void test_generateSoundex() {
+ 
+void testSoundex(const char *input, const char *expected) {
     char soundex[5];
-    
-    generateSoundex("Robert", soundex);
-    
-    generateSoundex("Jackson", soundex);
-    
-    generateSoundex("Li", soundex);
-
-    generateSoundex("O'Neill", soundex);
-    
-    generateSoundex("", soundex);
+    generateSoundex(input, soundex);
+}
+ 
+int main() {
+    testSoundex("Robert", "R163");
+    testSoundex("Rupert", "R163");
+    testSoundex("Aeiou", "A000");
+    testSoundex("1234", "1000");
+    testSoundex("J@hn!", "J500");
+    testSoundex("", "0000");
+    testSoundex("A", "A000");
+    testSoundex("Aaaaaa", "A000");
+    testSoundex("sOmeTeSt", "S530");
+    testSoundex("abcdefghijklmnopqrstuvwxyz", "A123");
+ 
+    return 0;
 }
